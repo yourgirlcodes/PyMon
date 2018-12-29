a9b8050e1e08dcc6164ebc7e58e03175af37df92
Gilad Navot <Gilad>
2018-12-29 19:59:49 +0200

    with

--- backend/dbutils.py
+++ backend/dbutils.py
@@ -3,9 +3,9 @@ import pymysql
 
 # Connect to the database
 connection = pymysql.connect(host='db4free.net',
-                             user='<your user>',
-                             password='<your password>',
-                             db='<your db>',
+                             user='slideshowbuilder',
+                             password='slideshowbuilder',
+                             db='slideshowbuilder',
                              charset='utf8',
                              autocommit=True,
                              cursorclass=pymysql.cursors.DictCursor)
